841ee0b25f31956e81e383a2cdca8eea4f27f25c
pranavvdesai <pranavdesai.psd@gmail.com>
2021-09-27 12:46:40 +0530

    init rootquery

--- schema/schema.js
+++ schema/schema.js
@@ -1,6 +1,6 @@
 const graphql = require('graphql');
 
-const {GrapQLObjectType, GraphQLString} = graphql;
+const {GrapQLObjectType, GraphQLString, GraphQLSchema} = graphql;
 
 const BookType = new GrapQLObjectType({
     name: 'Book',
@@ -13,3 +13,25 @@ const BookType = new GrapQLObjectType({
     
     })
 })
+
+const RootQuery = new GrapQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        book: {
+            type: BookType,
+            args: {id: {type: GraphQLString}},
+            resolve(parent, args){
+                // code to get data from db / other source
+                return {
+                    id: '123',
+                    name: 'Harry Potter',
+                    genre: 'Fantasy'
+                }
+            }
+        }
+    }
+})
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
+});
