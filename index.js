19be33ecc4056e231d641d615da88fa1f796238a
pranavvdesai <pranavdesai.psd@gmail.com>
2021-10-05 15:15:23 +0530

    mongo connection

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const {graphqlHTTP} = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const app = express();
+
+
+mongoose.connect(process.env.DB_USER , {useNewUrlParser: true});
+
+mongoose.connection.once('open', () => {
+    console.log('connected to database');
+});
+
+
 
 app.use('/graphql', graphqlHTTP({
     schema,
