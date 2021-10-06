5eb9968d42ebbf0c568623b6fd1c79d98dfe7990
pranavvdesai <pranavdesai.psd@gmail.com>
2021-10-06 14:27:29 +0530

    init components

---
+++ client/src/components/AuthorList/AuthorList.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function AuthorList() {
+    return (
+        <div>
+            <ul id="author-list">
+                <li>AaUTHOR name</li>
+            </ul>
+        </div>
+    )
+}
+
+export default AuthorList
